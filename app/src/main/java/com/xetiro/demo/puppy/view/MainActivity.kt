a623d551af6132ddbe61f70e0cb2f08a5174cbb0
xetiro <rubengeraldes@gmail.com>
2021-02-26 19:10:15 +0900

    Created the two fragment views and added a navhostfragment to handle the navigation between fragments.

--- app/src/main/java/com/xetiro/demo/puppy/view/MainActivity.kt
+++ app/src/main/java/com/xetiro/demo/puppy/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.xetiro.demo.puppy
+package com.xetiro.demo.puppy.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xetiro.demo.puppy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
